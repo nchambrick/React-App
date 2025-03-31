5a86e905f5f5537f502d93c4647c7782c9a75c66
nchambrick <nicolechildresshambrick@gmail.com>
2025-03-31 18:38:55 -0500

    Updating content

--- src/Layout.js
+++ src/Layout.js
@@ -5,7 +5,7 @@ function Layout({children})
 {
     return (
          <div className="container mt-4">
-            <h3 className="text-center mb-4">React LIST App</h3>
+            <h3 className="text-center mb-4">To-do List App</h3>
             <nav>
                 <Link className="btn btn-primary me-2" to="/"> Home </Link>
                 <Link className="btn btn-success" to="/add"> Add New </Link>
